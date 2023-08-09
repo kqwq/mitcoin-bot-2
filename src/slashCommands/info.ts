561ff519656ad1d752a3f515f93f15985d451b19
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-08-08 22:34:41 -0400

    better info

--- src/slashCommands/info.ts
+++ src/slashCommands/info.ts
@@ -5,6 +5,7 @@ import {
 } from "discord.js";
 import {
   COLORS,
+  blockchain,
   developmentGuildId,
   githubLink,
   googleSheetsViewLink,
@@ -25,10 +26,6 @@ export default {
     // Create embed
 
     const embed = new EmbedBuilder().setColor(COLORS.primary).addFields(
-      {
-        name: "Creator",
-        value: `@area5 and @potatos03, rewritten (with permission) by [@kqwq](https://kyle.ro)`,
-      },
       { name: "GitHub", value: `[mitcoin-bot-2](${githubLink})` },
       {
         name: "Backend",
@@ -39,8 +36,8 @@ export default {
         value: millisToFormattedString(process.uptime() * 1000),
       },
       {
-        name: "Support & View Blockchain",
-        value: `[Kyle's Web Experiments](https://discord.gg/${process.env.NEW_SERVER_INVITE})`,
+        name: "Blockchain Link",
+        value: `[Server](https://discord.gg/${process.env.ORIGINAL_SERVER_INVITE}) > [#blockchain](https://discord.com/channels/${blockchain.guildId}/${blockchain.channelId})`,
       }
     );
 
